e2b5c030b07471340f495474187b3efcb03b9088
LeviDeveloper001 <miridonovlev@gmail.com>
2024-12-05 12:04:10 +0300

    доработка профилей

--- aapp/utils/auth/mixins.py
+++ aapp/utils/auth/mixins.py
@@ -1,6 +1,9 @@
 from typing import Any
-from django.views.generic.base import ContextMixin
 
+from django.views.generic.base import ContextMixin
+from django.http import HttpRequest
+
+from accounts.models import Profile
 
 class AuthMixin(ContextMixin):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
@@ -9,4 +12,19 @@ class AuthMixin(ContextMixin):
         context['user']=user
         context['is_auth'] = user.is_authenticated
         return context
+
+
+
+class ProfileMixin(AuthMixin):
+    request:HttpRequest
+    profile_model=Profile
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['profile']=self.profile_model.manager.get(user=self.request.user)
+        else:
+            context['profile']=None
+        return context
+        
+
 
--- aapp/utils/general/mixins.py
+++ aapp/utils/general/mixins.py
@@ -1,9 +1,9 @@
 from typing import Any
-from utils.auth.mixins import AuthMixin
+from utils.auth.mixins import AuthMixin, ProfileMixin
 
 
 
-class GeneralMixin(AuthMixin):
+class GeneralMixin(ProfileMixin):
     '''Общий миксин для всех представлений в приложении'''
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
